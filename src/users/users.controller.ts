059fe04155efb4dd0900b37ff6bbb62849eee28a
Phan Thanh Hoàng <hoangphandev98@gmail.com>
2024-10-01 22:24:25 +0700

    Handle validate dto, hash password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,18 +9,15 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(
-    @Body('email') email: string,
-    @Body('password') password: string,
-    @Body('name') name: string,
-  ) {
-    return this.usersService.create(email, password, name);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Get()
@@ -30,7 +27,7 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
 
-  async create(email: string, password: string, name: string) {
-    const user = await this.userModel.create({ email, password, name });
+  private hashPassword = (password: string) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    return hash;
+  };
+
+  async create(createdUserDto: CreateUserDto) {
+    const hashPassword = this.hashPassword(createdUserDto.password);
+
+    const user = await this.userModel.create({
+      email: createdUserDto.email,
+      password: hashPassword,
+      name: createdUserDto.name,
+    });
     return user;
   }
 
@@ -17,8 +35,11 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'Not found user!';
+    }
+    return this.userModel.findOne({ _id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
